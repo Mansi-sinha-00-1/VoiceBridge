3f55b0f54608ade11fd082bc4273c367798e6807
mansi-sinha04 <sinhamansi04@gmail.com>
2026-05-01 13:08:01 +0530

    settings changes

--- voice_project/settings.py
+++ voice_project/settings.py
@@ -25,8 +25,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = [
+    "voicebridge-sreq.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 # Application definition
 

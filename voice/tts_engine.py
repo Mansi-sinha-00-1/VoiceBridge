5eb29112f5bb46e03f8b47eed45f6a228d2c83f9
mansi-sinha04 <sinhamansi04@gmail.com>
2026-05-02 10:50:22 +0530

    generator fixing again

--- voice/tts_engine.py
+++ voice/tts_engine.py
@@ -5,10 +5,14 @@ from gtts import gTTS
 
 def generate_voice(text, tone="normal", language="en"):
     audio_dir = os.path.join(settings.MEDIA_ROOT, "audio")
+
+    # Ensure directory exists
     os.makedirs(audio_dir, exist_ok=True)
 
     filename = f"{uuid.uuid4()}.mp3"
     full_path = os.path.join(audio_dir, filename)
+
+    print("Saving file to:", full_path)
 
     tts = gTTS(text=text, lang=language, slow=False)
     tts.save(full_path)
